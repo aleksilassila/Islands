4562d3bf5b2424fe58ab67c31b8c51e97acb561a
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2020-12-04 16:47:13 +0200

    Fixed a lot of protection issues

--- src/me/aleksilassila/islands/protection/ProtectionListeners.java
+++ src/me/aleksilassila/islands/protection/ProtectionListeners.java
@@ -6,6 +6,8 @@ import me.aleksilassila.islands.utils.Messages;
 import me.aleksilassila.islands.utils.Permissions;
 import org.bukkit.Material;
 import org.bukkit.Tag;
+import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,6 +16,7 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.*;
+import org.bukkit.event.vehicle.VehicleDestroyEvent;
 
 public class ProtectionListeners implements Listener {
     public ProtectionListeners() {
@@ -21,7 +24,8 @@ public class ProtectionListeners implements Listener {
     }
 
     @EventHandler
-    public void onEntityDamageEvent(EntityDamageByEntityEvent event) {
+    private void onEntityDamageEvent(EntityDamageByEntityEvent event) {
+        if (event.isCancelled()) return;
         if (event.getEntity().getWorld().equals(Islands.islandsWorld) && event.getDamager() instanceof Player) {
             if (event.getDamager().hasPermission(Permissions.bypass.interactEverywhere)) return;
 
@@ -41,7 +45,9 @@ public class ProtectionListeners implements Listener {
     }
 
     @EventHandler
-    public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event) {
+    private void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+        if (event.isCancelled()) return;
+
         if (event.getPlayer().getWorld().equals(Islands.islandsWorld)) {
             if (event.getPlayer().hasPermission(Permissions.bypass.interactEverywhere)) return;
 
@@ -50,7 +56,10 @@ public class ProtectionListeners implements Listener {
 
             String ownerUUID = IslandsConfig.getBlockOwnerUUID(x, z, event.getPlayer().hasPermission(Permissions.bypass.interactInPlot));
             if (ownerUUID != null && !ownerUUID.equals(event.getPlayer().getUniqueId().toString())) {
-                if (new ProtectedBlock(x, z).canOpenContainers(event.getPlayer().getUniqueId().toString())) return;
+                if (event.getRightClicked().getType() == EntityType.MINECART) {
+                    if (new ProtectedBlock(x, z).canUseUtility(event.getPlayer().getUniqueId().toString())) return;
+                }
+                else if (new ProtectedBlock(x, z).canOpenContainers(event.getPlayer().getUniqueId().toString())) return; // fixme armorstands work but what about others
 
                 event.setCancelled(true);
                 event.getPlayer().sendMessage(Messages.get("error.NOT_TRUSTED"));
@@ -59,16 +68,22 @@ public class ProtectionListeners implements Listener {
     }
 
     @EventHandler
-    public void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) {
+    private void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event) {
+        onPlayerInteractEntity(event);
+    }
+
+    @EventHandler
+    private void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) {
         onPlayerBucketEvent(event);
     }
 
     @EventHandler
-    public void onPlayerBucketFill(PlayerBucketFillEvent event) {
+    private void onPlayerBucketFill(PlayerBucketFillEvent event) {
         onPlayerBucketEvent(event);
     }
 
-    public void onPlayerBucketEvent(PlayerBucketEvent event) {
+    private void onPlayerBucketEvent(PlayerBucketEvent event) {
+        if (event.isCancelled()) return;
         if (event.getPlayer().getWorld().equals(Islands.islandsWorld)) {
             if (event.getPlayer().hasPermission(Permissions.bypass.interactEverywhere)) return;
 
@@ -86,13 +101,15 @@ public class ProtectionListeners implements Listener {
     }
 
     @EventHandler // Player interact restriction
-    public void onPlayerInteract(PlayerInteractEvent event) {
+    private void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.isCancelled()) return;
         if (event.getClickedBlock() == null) return;
         if (event.getPlayer().hasPermission(Permissions.bypass.interactEverywhere)) return;
         if (!event.getPlayer().getWorld().equals(Islands.islandsWorld)) return;
 
-        int x = event.getClickedBlock().getX();
-        int z = event.getClickedBlock().getZ();
+        Block clickedBlock = event.getClickedBlock();
+        int x = clickedBlock.getX();
+        int z = clickedBlock.getZ();
 
         Player player = event.getPlayer();
         String ownerUUID = IslandsConfig.getBlockOwnerUUID(x, z, player.hasPermission(Permissions.bypass.interactInPlot));
@@ -105,68 +122,78 @@ public class ProtectionListeners implements Listener {
         if (protectedBlock.canDoAnything(player.getUniqueId().toString())) return;
 
         if (event.getAction() == Action.PHYSICAL) {
-            if (Tag.PRESSURE_PLATES.isTagged(event.getClickedBlock().getType()) &&
+            if (Tag.PRESSURE_PLATES.isTagged(clickedBlock.getType()) &&
                     !protectedBlock.canUseDoors(player.getUniqueId().toString())) {
                 event.setCancelled(true);
-            } else if (event.getClickedBlock().getType() == Material.FARMLAND &&
+            } else if (clickedBlock.getType() == Material.FARMLAND &&
                     !protectedBlock.canDoAnything(player.getUniqueId().toString())) {
                 event.setCancelled(true);
             }
         }
 
+        // These need permission to do anything
+        if (clickedBlock.getType() == Material.TNT ||
+                Tag.RAILS.isTagged(clickedBlock.getType()) ||
+                (event.getItem() != null && event.getItem().getType() == Material.BONE_MEAL)) {
+            event.setCancelled(true);
+            Messages.send(event.getPlayer(), "error.NOT_TRUSTED");
+            return;
+        }
+
         // Doors
-        if (Tag.WOODEN_DOORS.isTagged(event.getClickedBlock().getType()) ||
-                Tag.WOODEN_TRAPDOORS.isTagged(event.getClickedBlock().getType()) ||
-                Tag.FENCE_GATES.isTagged(event.getClickedBlock().getType()))
+        if (Tag.WOODEN_DOORS.isTagged(clickedBlock.getType()) ||
+                Tag.WOODEN_TRAPDOORS.isTagged(clickedBlock.getType()) ||
+                Tag.FENCE_GATES.isTagged(clickedBlock.getType()))
         {
             if (!protectedBlock.canUseDoors(player.getUniqueId().toString())) {
                 event.setCancelled(true);
-                event.getPlayer().sendMessage(Messages.get("error.NOT_TRUSTED"));
+                Messages.send(event.getPlayer(), "error.NOT_TRUSTED");
             }
         }
 
         // Containers
-        else if (event.getClickedBlock().getType() == Material.CHEST ||
-                event.getClickedBlock().getType() == Material.TRAPPED_CHEST ||
-                event.getClickedBlock().getType() == Material.CHEST_MINECART ||
-                event.getClickedBlock().getType() == Material.FURNACE ||
-                event.getClickedBlock().getType() == Material.BLAST_FURNACE ||
-                event.getClickedBlock().getType() == Material.FURNACE_MINECART ||
-                event.getClickedBlock().getType() == Material.HOPPER ||
-                event.getClickedBlock().getType() == Material.HOPPER_MINECART ||
-                event.getClickedBlock().getType() == Material.DISPENSER ||
-                event.getClickedBlock().getType() == Material.DROPPER ||
-                event.getClickedBlock().getType() == Material.JUKEBOX ||
-                event.getClickedBlock().getType() == Material.BARREL ||
-                Tag.SHULKER_BOXES.isTagged(event.getClickedBlock().getType()))
+        else if (clickedBlock.getType() == Material.CHEST ||
+                clickedBlock.getType() == Material.TRAPPED_CHEST ||
+                clickedBlock.getType() == Material.CHEST_MINECART ||
+                clickedBlock.getType() == Material.FURNACE ||
+                clickedBlock.getType() == Material.BLAST_FURNACE ||
+                clickedBlock.getType() == Material.FURNACE_MINECART ||
+                clickedBlock.getType() == Material.HOPPER ||
+                clickedBlock.getType() == Material.HOPPER_MINECART ||
+                clickedBlock.getType() == Material.DISPENSER ||
+                clickedBlock.getType() == Material.DROPPER ||
+                clickedBlock.getType() == Material.JUKEBOX ||
+                clickedBlock.getType() == Material.BARREL ||
+                clickedBlock.getType() == Material.SMOKER ||
+                Tag.SHULKER_BOXES.isTagged(clickedBlock.getType()))
         {
             if (!protectedBlock.canOpenContainers(player.getUniqueId().toString())) {
                 event.setCancelled(true);
-                event.getPlayer().sendMessage(Messages.get("error.NOT_TRUSTED"));
+                Messages.send(event.getPlayer(), "error.NOT_TRUSTED");
             }
         }
         // Utility
-        else if (Tag.CAMPFIRES.isTagged(event.getClickedBlock().getType()) ||
-                Tag.BEDS.isTagged(event.getClickedBlock().getType()) ||
-                event.getClickedBlock().getType() == Material.NOTE_BLOCK ||
-                event.getClickedBlock().getType() == Material.CAULDRON ||
-                event.getClickedBlock().getType() == Material.ANVIL ||
-                event.getClickedBlock().getType() == Material.CHIPPED_ANVIL ||
-                event.getClickedBlock().getType() == Material.DAMAGED_ANVIL ||
-                event.getClickedBlock().getType() == Material.CAKE ||
-                event.getClickedBlock().getType() == Material.SWEET_BERRY_BUSH ||
-                event.getClickedBlock().getType() == Material.BEE_NEST ||
-                event.getClickedBlock().getType() == Material.BEEHIVE ||
-                event.getClickedBlock().getType() == Material.BEACON ||
-                event.getClickedBlock().getType() == Material.BELL ||
-                event.getClickedBlock().getType() == Material.CARTOGRAPHY_TABLE ||
-                event.getClickedBlock().getType() == Material.LOOM ||
-                event.getClickedBlock().getType() == Material.MINECART ||
-                event.getClickedBlock().getType() == Material.RESPAWN_ANCHOR)
+        else if (Tag.CAMPFIRES.isTagged(clickedBlock.getType()) ||
+                Tag.BEDS.isTagged(clickedBlock.getType()) ||
+                clickedBlock.getType() == Material.NOTE_BLOCK ||
+                clickedBlock.getType() == Material.CAULDRON ||
+                clickedBlock.getType() == Material.ANVIL ||
+                clickedBlock.getType() == Material.CHIPPED_ANVIL ||
+                clickedBlock.getType() == Material.DAMAGED_ANVIL ||
+                clickedBlock.getType() == Material.CAKE ||
+                clickedBlock.getType() == Material.SWEET_BERRY_BUSH ||
+                clickedBlock.getType() == Material.BEE_NEST ||
+                clickedBlock.getType() == Material.BEEHIVE ||
+                clickedBlock.getType() == Material.BEACON ||
+                clickedBlock.getType() == Material.BELL ||
+                clickedBlock.getType() == Material.CARTOGRAPHY_TABLE ||
+                clickedBlock.getType() == Material.LOOM ||
+                clickedBlock.getType() == Material.MINECART ||
+                clickedBlock.getType() == Material.RESPAWN_ANCHOR)
         {
             if (!protectedBlock.canUseUtility(player.getUniqueId().toString())) {
                 event.setCancelled(true);
-                event.getPlayer().sendMessage(Messages.get("error.NOT_TRUSTED"));
+                Messages.send(event.getPlayer(), "error.NOT_TRUSTED");
             }
         }
     }
@@ -208,6 +235,29 @@ public class ProtectionListeners implements Listener {
             } else if (!(new ProtectedBlock(x, z).canDoAnything(event.getPlayer().getUniqueId().toString()))) {
                 event.setCancelled(true);
                 event.getPlayer().sendMessage(Messages.get("error.NOT_TRUSTED"));
+            }
+        }
+    }
+
+    // MINECARTS
+    @EventHandler
+    private void on(VehicleDestroyEvent event) {
+        if (event.isCancelled()) return;
+        if (!(event.getAttacker() instanceof Player)) return;
+
+        Player player = (Player) event.getAttacker();
+        if (player.getWorld().equals(Islands.islandsWorld)) {
+            if (player.hasPermission(Permissions.bypass.interactEverywhere)) return;
+
+            int x = event.getVehicle().getLocation().getBlockX();
+            int z = event.getVehicle().getLocation().getBlockZ();
+
+            String ownerUUID = IslandsConfig.getBlockOwnerUUID(x, z, player.hasPermission(Permissions.bypass.interactInPlot));
+            if (ownerUUID != null && !ownerUUID.equals(player.getUniqueId().toString())) {
+                if (new ProtectedBlock(x, z).canDoAnything(player.getUniqueId().toString())) return;
+
+                event.setCancelled(true);
+                player.sendMessage(Messages.get("error.NOT_TRUSTED"));
             }
         }
     }
